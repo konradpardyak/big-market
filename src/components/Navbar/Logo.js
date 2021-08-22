a1ab5d8f9ab4486720d610d896bbd82860693711
Konrad Pardyak <konradpardyak@gmail.com>
2021-08-22 18:29:58 +0200

    Update Logo

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -3,30 +3,39 @@ import LogoIcon from '../icons/LogoIcon';
 
 const StyledLogo = styled.div`
   display: flex;
-  align-items: center;
-  flex-grow: 1;
-  @media (min-width: 600px) {
-    justify-content: center;
-  }
-  `
+`
+const LogoPic = styled.div`
+  background: ${props => props.theme.gradients.main};
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: .5rem;
+`
 const StyledHeader = styled.h1`
+  display: flex;
+  flex-direction: column;
+  padding-left: .25rem;
   color: ${props => props.theme.colors.main};
   font-style: italic;
-  font-size: 1rem;
-  line-height: 1rem;
+  font-weight: 800;
 `
 const StyledBig = styled.span`
-  font-weight: 800;
-  font-size: 2.5rem;
-  line-height: 2.5rem;
+  font-size: 2rem;
+  line-height: 1.75rem;
+`
+const StyledSmall = styled.span`
+  font-size: .75rem;
+  line-height: .75rem;
 `
 
 const Logo = () => {
   return(
     <StyledLogo>
-      <LogoIcon size="2.5rem" />
+      <LogoPic>
+        <LogoIcon size="2.5rem" />
+      </LogoPic>
       <StyledHeader>
-        <StyledBig>big</StyledBig>market
+        <StyledBig>BIG</StyledBig>
+        <StyledSmall>MARKET</StyledSmall>
       </StyledHeader>
     </StyledLogo>
   )
